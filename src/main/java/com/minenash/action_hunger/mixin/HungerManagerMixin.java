9566f5ece587afb5625484053841e5e43f69ee3e
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2025-10-05 19:11:25 +1300

    Remove redirect from HungerManagerMixin

--- src/main/java/com/minenash/action_hunger/mixin/HungerManagerMixin.java
+++ src/main/java/com/minenash/action_hunger/mixin/HungerManagerMixin.java
@@ -1,5 +1,6 @@
 package com.minenash.action_hunger.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.minenash.action_hunger.ActionHunger;
 import com.minenash.action_hunger.config.Config;
 import net.minecraft.entity.player.HungerManager;
@@ -12,7 +13,6 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(HungerManager.class)
 public abstract class HungerManagerMixin {
@@ -22,16 +22,20 @@ public abstract class HungerManagerMixin {
     @Shadow private float saturationLevel;
     @Shadow private int foodTickTimer;
     @Shadow public abstract void addExhaustion(float exhaustion);
-    @Shadow private void addInternal(int food, float exhaustion) {}
 
     @Unique private int constantRegenTimer = 0;
     @Unique private int constantHungerTimer = 0;
     @Unique private int shieldExhaustionTimer = 0;
 
 
-    @Redirect(method = "eat", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addInternal(IF)V"))
-    private void actionHunger$modifyFoodComponent(HungerManager manager, int food, float saturation) {
-        addInternal(Math.round(food * Config.hungerFromFoodMultiplier), saturation * Config.saturationFromFoodMultiplier);
+    @ModifyExpressionValue(method = "eat", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/type/FoodComponent;nutrition()I"))
+    private int actionHunger$modifyFoodNutrition(int nutrition) {
+        return (int) (nutrition * Config.hungerFromFoodMultiplier);
+    }
+
+    @ModifyExpressionValue(method = "eat", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/type/FoodComponent;saturation()F"))
+    private float actionHunger$modifyFoodSaturation(float saturation) {
+        return saturation * Config.saturationFromFoodMultiplier;
     }
 
     /**
